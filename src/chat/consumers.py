9a1bea32210e9cb840d95bc2ddda961b4cadc04d
neeraj <shuklaneeraj2014.2015@gmail.com>
2018-06-08 04:46:07 +0530

    Sending and recieving message through websocket

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,3 +1,4 @@
+import json
 from channels.consumer import SyncConsumer
 
 
@@ -5,10 +6,20 @@ class ChatConsumer(SyncConsumer):
     def websocket_connect(self, event):
         # when the socket connects
         print(event)
+        self.send({
+            "type": "websocket.accept"
+        })
 
-    def websocket_receive(self, event):
+
+    def websocket_receive(self, event): # websocket.receive
         # when the socket connects
-        print(event)
+        print(event['text'])
+        new_message_data = json.loads(event['text'])
+        print(new_message_data)
+        self.send({
+            "type": "websocket.send",
+            "text": json.dumps(new_message_data)
+        })
 
     def websocket_disconnect(self, event):
         # when the socket connects
