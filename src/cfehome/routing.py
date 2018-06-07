ca24c9339c29f6bf70ac5445d86f664dc4040620
neeraj <shuklaneeraj2014.2015@gmail.com>
2018-06-08 04:31:43 +0530

    Setup for Django channel and Websockket routing

---
+++ src/cfehome/routing.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+application = ProtocolTypeRouter({
+    # Websocket chat handler
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                # url(r"chat/", ChatConsumer, name='chat')
+                # url(r"chat/(?P<username>[\w.@+-]+)", ChatConsumer, name='chat')
+                ]
+            )
+        ),
+    )
+})
